d3ed2edea3975c6465268f1323a9d4e6e5396dd9
wkomorowski <wkomorowski@sii.pl>
2025-02-09 18:15:26 +0100

    Restart repo

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightTrello2nd;
+
+public class Tests: PageTest
+{
+    [SetUp]
+    public async Task Setup()
+    {
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
